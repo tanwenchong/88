619ca347ecfaa68ebf6fedabf6535514c407484a
tanwenchong <129516120+tanwenchong@users.noreply.github.com>
2025-02-20 11:26:14 +0800

    Update get_siRNA.py

--- ENsiRNA/get_siRNA.py
+++ ENsiRNA/get_siRNA.py
@@ -2,7 +2,7 @@ import argparse
 import pandas as pd
 from Bio.Seq import Seq
 import re
-#python get_siRNA.py -i /public2022/tanwenchong/rna/EnSIRNA-main/data/mRNA.fasta -o 
+
 parser=argparse.ArgumentParser()
 parser.add_argument('-i','--input',type=str,required=True,help='input mrna fasta file')
 parser.add_argument('-s','--sirna', type=str, nargs='+', default=None)
@@ -26,9 +26,6 @@ if args.sirna is None:
         mrna=mrna.replace('T','U')
         for i in range(len(mrna)-18):
                 sense=mrna[i:i+19] #str
-                #超过6个重复
-                if len(re.findall(r'(.)\1{6,}', sense))>0:
-                    continue
                 anti = str(Seq(sense).reverse_complement())
                 siRNA_id=mrna_id+'_'+str(i)
                 siRNA.append([siRNA_id,anti,sense,mrna,i,0])
@@ -45,7 +42,7 @@ else:
 df=pd.DataFrame(siRNA,columns=columns)
 df.to_csv(f'{args.output}/{args.input.split("/")[-1].split(".")[0]}.csv',index=False)
 csv_path=f'{args.output}/{args.input.split("/")[-1].split(".")[0]}.csv'
-#输出给bash
+
 print(csv_path)
 
 
